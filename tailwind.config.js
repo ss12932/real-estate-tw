68792f826957c1f48c0f34b64b4a658ac292dfbd
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-05-17 11:46:45 +0100

    created nav component

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      colors: {
+        "sand-yellow": "#fffac0",
+        "sunset-orange": "#ffd79a",
+        "sea-blue": "#73b9d7",
+        "sky-blue": "#9de6e8",
+      },
+    },
+  },
+  plugins: [],
+};
